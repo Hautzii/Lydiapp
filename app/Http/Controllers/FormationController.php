9c44b803b1bff849042ff0e1f910079a43c59d97
Arthur <am.03012002@gmail.com>
2024-04-25 15:16:50 +0200

    commit

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\formation;
+use App\Models\Formation;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
