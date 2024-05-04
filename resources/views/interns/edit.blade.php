301969a958e26452a4ed39e01339717184de7640
Arthur <am.03012002@gmail.com>
2024-05-04 20:29:06 +0200

    stylized interns inputs

--- resources/views/interns/edit.blade.php
+++ resources/views/interns/edit.blade.php
@@ -19,9 +19,9 @@
                                 <input type="file" id="profile_picture" name="profile_picture" class="text-center mb-4">
                             </div>
                             <label for="first_name" class="self-start text-center w-full">Prénom :</label>
-                            <input type="text" id="first_name" name="first_name" value="{{$intern->first_name}}" class="mb-4 mt-2 py-1 text-center rounded w-72 border border-gray-900">
+                            <input type="text" id="first_name" name="first_name" value="{{$intern->first_name}}" class="capitalize mb-4 mt-2 py-1 text-center rounded w-72 border border-gray-900">
                             <label for="last_name" class="self-start text-center w-full">Nom :</label>
-                            <input type="text" id="last_name" name="last_name" value="{{$intern->last_name}}" class="mb-4 mt-2 py-1 text-center rounded w-72 border border-gray-900">
+                            <input type="text" id="last_name" name="last_name" value="{{$intern->last_name}}" class="uppercase mb-4 mt-2 py-1 text-center rounded w-72 border border-gray-900">
                             <label for="phone_number" class="self-start text-center w-full">Numéro de téléphone :</label>
                             <input type="text" id="phone_number" name="phone_number" value="{{$intern->phone_number}}" class="mb-4 mt-2 py-1 text-center rounded w-72 border border-gray-900">
                             <label for="email" class="self-start text-center w-full">Email :</label>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,29 @@
 module.exports = {
-  purge: {
-    content: [
-      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-      './storage/framework/views/*.php',
-      './resources/views/**/*.blade.php',
-    ],
-    safelist: [],
-  },
-  theme: {
-    extend: {
-      colors: {
-        'button': '#838cf1'
-      },
-      textTransform: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-      fontFamily: {
-        sans: ['Figtree', 'Arial', 'sans-serif'],
-      },
-      spacing: {
-        'search': '0.625rem',
-      },
+    purge: {
+        content: [
+            "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+            "./storage/framework/views/*.php",
+            "./resources/views/**/*.blade.php",
+        ],
+        safelist: [],
     },
-  },
-  plugins: [],
-}
+    theme: {
+        extend: {
+            colors: {
+                button: "#838cf1",
+            },
+            textTransform: {
+                uppercase: "uppercase",
+                lowercase: "lowercase",
+                capitalize: "capitalize",
+            },
+            fontFamily: {
+                sans: ["Figtree", "Arial", "sans-serif"],
+            },
+            spacing: {
+                search: "0.625rem",
+            },
+        },
+    },
+    plugins: [],
+};
