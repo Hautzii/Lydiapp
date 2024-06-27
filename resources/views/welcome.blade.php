c89aa9fd42f87cc0c33033b1a1d123c24b4b84a1
Hautzii <74426817+Hautzii@users.noreply.github.com>
2024-06-27 15:16:49 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Trombinoscope pour les stagiaires et les formations du centre M2i de Villeneuve d'Ascq.">
 
         <title>Lydiapp</title>
 
